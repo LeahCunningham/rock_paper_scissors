c642906eb23448a2a76b350923f815e4546bccb1
LeahCunningham <71873751+LeahCunningham@users.noreply.github.com>
2025-08-06 15:21:42 +0800

    Add functions to get Human and Computer choices
    Randomly assigns options for the computer responses and gets human input via prompt

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-// 
+
 // There are three choices - rock, paper, scissors
-// Randomly select one of the choices as the computer choice
-    // If the random number is between 0 and 0.3
+// Randomly select one of the 3 choices for the computer player
+    // Generate a random number that results in 3 possible integer values
+    // Assign each of the 3 possible integers to each of the 3 choice options
 // Allow the user to select one of the choices for their choice
-// Create variables to store the score for the user and the computer
 // Compare the choice of the computer and the user to see who won
+// Store the score for the user and the computer
+    // Create variables for storing these values
+
+
+
+
+let randomChoice = Math.floor(Math.random() * 3); // returns a random number that results in a vaule of 0, 1 or 2
 
 function getComputerChoice () {
-    return Math.floor(Math.random() * 3);
-        
+        if (randomChoice < 1) {
+            return "rock";
+        } 
+        else if (randomChoice < 2 && randomChoice >= 1) {
+            return "paper";
+        }
+        else {
+            return "scissors";
+        }
 }
+
+let humanChoice = prompt ("Choose your fighter - Rock, Paper or Scissors");
+
+function getHumanChoice () {
+        if (humanChoice.toLowerCase() === "rock") {
+            return "rock";
+        }
+         else if (humanChoice.toLowerCase() === "paper") {
+            return "paper";
+        }
+         else if (humanChoice.toLowerCase() === "scissors") {
+            return "scissors";
+        }
+         else {
+            return "please only enter 'rock' 'paper' or 'scissors'";
+        }
+}
+
+console.log(getComputerChoice())
+
+console.log(getHumanChoice())
