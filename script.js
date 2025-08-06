bda95e1630e535567aebc8362924f61e087a523d
LeahCunningham <71873751+LeahCunningham@users.noreply.github.com>
2025-08-06 14:35:50 +0800

    Add initial files
    First save with beginnings of pseudocode

---
+++ script.js
@@ -0,0 +1,12 @@
+// 
+// There are three choices - rock, paper, scissors
+// Randomly select one of the choices as the computer choice
+    // If the random number is between 0 and 0.3
+// Allow the user to select one of the choices for their choice
+// Create variables to store the score for the user and the computer
+// Compare the choice of the computer and the user to see who won
+
+function getComputerChoice () {
+    return Math.floor(Math.random() * 3);
+        
+}
